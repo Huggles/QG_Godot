fe5dbfab41feaf55ca1fc423a2bec9bdafd75895
Hugo van Krimpen <HugoVK15@gmail.com>
2026-08-23 22:44:37 +0200

    Changed blinking of supply icon to a shader

--- scenes/Units/BlinkingSprite2D.cs
+++ scenes/Units/BlinkingSprite2D.cs
@@ -1,52 +1,52 @@
 using Godot;
 using System;
 
-public partial class BlinkingSprite2D : Sprite2D
+public partial class BlinkingSprite2D : TexturedSprite2D
 {
-    private Tween _tween;
-    private bool _isBlinking = false;
+	private Tween _tween;
+	private bool _isBlinking = false;
 
-    public override void _Ready()
-    {
-        Visible = false;
-        _tween = CreateTween();
-        _tween.SetTrans(Tween.TransitionType.Linear);
-        _tween.SetEase(Tween.EaseType.InOut);
-        _tween.SetLoops();
-        _tween.TweenProperty(this, "modulate:a", 0.5f, GameSettings.DurationLongSeconds).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
-        _tween.TweenProperty(this, "modulate:a", 1.0f, GameSettings.DurationLongSeconds).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
-        _tween.Stop(); // Start with blinking stopped
-    }
+	public override void _Ready()
+	{
+		Visible = false;
+		_tween = CreateTween();
+		_tween.SetTrans(Tween.TransitionType.Linear);
+		_tween.SetEase(Tween.EaseType.InOut);
+		_tween.SetLoops();
+		_tween.TweenProperty(this, "modulate:a", 0.5f, GameSettings.DurationLongSeconds).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+		_tween.TweenProperty(this, "modulate:a", 1.0f, GameSettings.DurationLongSeconds).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+		_tween.Stop(); // Start with blinking stopped
+	}
 
-    public void StartBlinking()
-    {
-        if (!_isBlinking)
-        {
-            _tween.Play();
-            _isBlinking = true;
-        }
-    }
+	public void StartBlinking()
+	{
+		if (!_isBlinking)
+		{
+			_tween.Play();
+			_isBlinking = true;
+		}
+	}
 
-    public void StopBlinking()
-    {
-        if (_isBlinking)
-        {
-            
-            _tween.Stop();
-            Modulate = new Color(this.Modulate.R, this.Modulate.G, this.Modulate.B, 1); // Reset alpha to fully visible
-            _isBlinking = false;
-        }
-    }
+	public void StopBlinking()
+	{
+		if (_isBlinking)
+		{
+			
+			_tween.Stop();
+			Modulate = new Color(this.Modulate.R, this.Modulate.G, this.Modulate.B, 1); // Reset alpha to fully visible
+			_isBlinking = false;
+		}
+	}
 
-    public new void Show()
-    {
-        Visible = true;
-        StartBlinking();
-    }
+	public new void Show()
+	{
+		Visible = true;
+		StartBlinking();
+	}
 
-    public new void Hide()
-    {
-        StopBlinking();
-        Visible = false;
-    }
+	public new void Hide()
+	{
+		StopBlinking();
+		Visible = false;
+	}
 }
